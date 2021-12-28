ffeb708a4f1904f1ceb889eae520a760af0ef0f9
noinoi-X <94881832+BaziiX@users.noreply.github.com>
2021-12-28 16:34:37 +0530

    Update config.py

--- Noinoi/config.py
+++ Noinoi/config.py
@@ -1,1 +1,12 @@
+##Config
 
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
