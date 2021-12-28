4f6d8647e3071a023f663429239af105d1ef54f1
noinoi-X <94881832+BaziiX@users.noreply.github.com>
2021-12-28 14:52:45 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,47 @@
+<h2 align= center><b>🎧 47Music Player 🎧</b></h2>
+<h4 align = center>A Powerful and Simple Telegram Bot<br> that can play songs & videos in Telegram group voice chats</h4>
+
+<p align='center'>
+<a href="https://www.python.org/" alt="made-with-python"> <img src="https://img.shields.io/badge/Made%20with-Python-1f425f.svg?style=flat-square&logo=python&color=blue"></a>
+<a href="https://github.com/ImJanindu/47MusicPlayerBot/graphs/commit-activity" alt="Maintenance"> <img src="https://img.shields.io/badge/Maintained%3F-yes-green.svg?style=flat-square"></a></p>
+
+<p align="center"><a href="https://t.me/JaguarBots"><img src="https://telegra.ph/file/4c03ff6d25fc678a2fe08.jpg" height="300" width="310" alt="Music Player"></p>
+
+## ✨ Heroku Deploy
+Change the app region to Europe (it will help to make the bot stable)
+  
+<p align="left"><a href="https://dashboard.heroku.com/new?template=https://github.com/ImJanindu/47MusicPlayerBot"><img src="https://img.shields.io/badge/Deploy%20To%20Heroku-blueviolet?style=for-the-badge&logo=heroku" width="250" alt="Deploy"></a></p>
+
+## 🏃‍♂ Pyrogram Session
+
+<p align="left"><a href="https://replit.com/@AaravxD/PyroStringSession#main.py"><img src="https://img.shields.io/badge/Generate%20On%20Repl-blueviolet?style=for-the-badge&logo=appveyor" width="245" alt="Repl"></a></p>  
+
+## 🛠 Config Vars
+
+1. `API_ID` : Assistant account Telegram API_ID, get it from my.telegram.org
+2. `API_HASH` : Assistant account Telegram API_HASH, get it from my.telegram.org
+3. `BOT_TOKEN` : Your Telegram bot token, get it from @BotFather
+4. `SESSION_NAME` : Pyrogram session string of assistant account
+5. `OWNER_ID` : Your Telegram ID
+
+## ℹ️ Commands
+
+| Command  | Description                                          |
+| -------  | ---------------------------------------------------- |
+| /play    | play the given query or YouTube link as audio        |
+| /video   | play the given query or YouTube link as video        |
+| /pause   | pause the audio stream                               |
+| /resume  | resume the audio stream                              |
+| /mute    | mute the userbot                                     |
+| /unmute  | unmute the userbot                                   |
+| /stop    | stop streaming and remove the userbot from the call  |
+| /restart | restart the bot                                      |
+
+## ❤️ Support
+
+<a href="https://t.me/JaguarBotsChat"><img src="https://img.shields.io/badge/Join-Telegram%20Group-blue.svg?logo=telegram" alt="Group"></a><br>
+<a href="https://t.me/JaguarBots"><img src="https://img.shields.io/badge/Join-Telegram%20Channel-red.svg?logo=Telegram" alt="Channel"></a>
+
+## 📄 License
+
+Distributed under the [MIT Licence.](https://github.com/ImJanindu/47MusicPlayer/blob/main/LICENSE) See `LICENSE.md` for more information.
---
+++ app.py
@@ -0,0 +1,330 @@
+"""
+MIT License
+
+Copyright (c) 2021 Janindu Malshan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import os
+import logging
+from pytube import YouTube
+from youtube_search import YoutubeSearch
+from pytgcalls import PyTgCalls, idle
+from pytgcalls.types import AudioPiped, AudioVideoPiped, GroupCall
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+
+bot = Client(
+    "Music Stream Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+client = Client(os.environ["SESSION_NAME"], int(os.environ["API_ID"]), os.environ["API_HASH"])
+
+app = PyTgCalls(client)
+
+CHATS = []
+
+OWNER_ID = int(os.environ["OWNER_ID"])
+
+START_TEXT = """
+Hi <b>{}</b> 👋
+I can play music in Telegram group voice chats. 
+
+<i>Only my owner can operate me. Make your own bot from the source code.</i>
+"""
+
+START_BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("📨 Support", url="https://t.me/JaguarBots"),
+            InlineKeyboardButton("📚 Source Code", url="https://github.com/ImJanindu/47MusicPlayerBot")
+        ]
+    ]
+)
+
+BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("⏸", callback_data="pause"),
+            InlineKeyboardButton("▶️", callback_data="resume"),
+            InlineKeyboardButton("⏹", callback_data="stop"),
+            InlineKeyboardButton("🔇", callback_data="mute"),
+            InlineKeyboardButton("🔊", callback_data="unmute")
+        ],
+        [
+            InlineKeyboardButton("🗑 Close Menu", callback_data="close")
+        ]
+    ]
+)
+
+
+@bot.on_callback_query()
+async def callbacks(_, cq: CallbackQuery): 
+    if cq.from_user.id != OWNER_ID:
+        return await cq.answer("You aren't the owner of me.")   
+    chat_id = cq.message.chat.id
+    data = cq.data
+    if data == "close":
+        return await cq.message.delete()
+    if not str(chat_id) in CHATS:
+        return await cq.answer("Nothing is playing.")
+
+    if data == "pause":
+        try:
+            await app.pause_stream(chat_id)
+            await cq.answer("Paused streaming.")
+        except:
+            await cq.answer("Nothing is playing.")
+      
+    elif data == "resume":
+        try:
+            await app.resume_stream(chat_id)
+            await cq.answer("Resumed streaming.")
+        except:
+            await cq.answer("Nothing is playing.")   
+
+    elif data == "stop":
+        await app.leave_group_call(chat_id)
+        CHATS.clear()
+        await cq.answer("Stopped streaming.")  
+
+    elif data == "mute":
+        try:
+            await app.mute_stream(chat_id)
+            await cq.answer("Muted streaming.")
+        except:
+            await cq.answer("Nothing is playing.")
+            
+    elif data == "unmute":
+        try:
+            await app.unmute_stream(chat_id)
+            await cq.answer("Unmuted streaming.")
+        except:
+            await cq.answer("Nothing is playing.")
+            
+
+@bot.on_message(filters.command("start") & filters.private)
+async def start_private(_, message):
+    msg = START_TEXT.format(message.from_user.mention)
+    await message.reply_text(text = msg,
+                             reply_markup = START_BUTTONS)
+    
+
+@bot.on_message(filters.command("start") & filters.group)
+async def start_group(_, message):
+    await message.reply_text("🎧 <i>Music player is running.</i>")
+    
+
+@bot.on_message(filters.command("play") & filters.group)
+async def music_play(_, message):
+    await message.delete()
+    user_id = message.from_user.id
+    if user_id != OWNER_ID:
+        return
+    try:
+        query = message.text.split(None, 1)[1]
+    except:
+        return await message.reply_text("<b>Usage:</b> <code>/play [query]</code>")
+    chat_id = message.chat.id
+    m = await message.reply_text("🔄 Processing...")
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        thumb = results[0]["thumbnails"][0]
+        duration = results[0]["duration"]
+        yt = YouTube(link)
+        cap = f"🎵 <b>Playing:</b> [{yt.title}]({link}) \n\n⏳ <b>Duration:</b> {duration}"
+        aud = yt.streams.get_by_itag(140).download()
+    except Exception as e:
+        if "Too Many Requests" in str(e):
+            await m.edit("❗️<i>Please wait at least 30 seconds to use me.</i>")
+            os.system(f"kill -9 {os.getpid()} && python3 app.py")
+        else:
+            return await m.edit(str(e))
+    
+    try:
+        if str(chat_id) in CHATS:
+            await app.change_stream(
+                chat_id,
+                AudioPiped(aud)
+            )
+            await message.reply_photo(thumb, caption=cap, reply_markup=BUTTONS)
+            await m.delete()
+            os.remove(aud)
+        else:            
+            await app.join_group_call(
+                chat_id,
+                AudioPiped(aud)
+            )
+            CHATS.append(str(chat_id))
+            await message.reply_photo(thumb, caption=cap, reply_markup=BUTTONS)
+            await m.delete()
+            os.remove(aud)
+    except Exception as e:
+        return await m.edit(str(e))
+    
+    
+@bot.on_message(filters.command("video") & filters.group)
+async def video_play(_, message):
+    await message.delete()
+    user_id = message.from_user.id
+    if user_id != OWNER_ID:
+        return
+    try:
+        query = message.text.split(None, 1)[1]
+    except:
+        return await message.reply_text("<b>Usage:</b> <code>/video [query]</code>")
+    chat_id = message.chat.id
+    m = await message.reply_text("🔄 Processing...")
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        thumb = results[0]["thumbnails"][0]
+        duration = results[0]["duration"]
+        yt = YouTube(link)
+        cap = f"🎬 <b>Playing:</b> [{yt.title}]({link}) \n\n⏳ <b>Duration:</b> {duration}"
+        vid = yt.streams.get_by_itag(22).download()
+    except Exception as e:
+        if "Too Many Requests" in str(e):
+            await m.edit("❗️<i>Please wait at least 30 seconds to use me.</i>")
+            os.system(f"kill -9 {os.getpid()} && python3 app.py")
+        else:
+            return await m.edit(str(e))
+    
+    try:
+        if str(chat_id) in CHATS:
+            await app.change_stream(
+                chat_id,
+                AudioVideoPiped(vid)
+            )
+            await message.reply_photo(thumb, caption=cap, reply_markup=BUTTONS)
+            await m.delete()
+            os.remove(vid)
+        else:            
+            await app.join_group_call(
+                chat_id,
+                AudioVideoPiped(vid)
+            )
+            CHATS.append(str(chat_id))
+            await message.reply_photo(thumb, caption=cap, reply_markup=BUTTONS)
+            await m.delete()
+            os.remove(vid)
+    except Exception as e:
+        return await m.edit(str(e))
+    
+
+@bot.on_message(filters.command("stop") & filters.group)
+async def end(_, message):
+    await message.delete()
+    user_id = message.from_user.id
+    if user_id != OWNER_ID:
+        return
+    chat_id = message.chat.id
+    if str(chat_id) in CHATS:
+        await app.leave_group_call(chat_id)
+        CHATS.clear()
+        await message.reply_text("⏹ Stopped streaming.")
+    else:
+        await message.reply_text("❗Nothing is playing.")
+        
+
+@bot.on_message(filters.command("pause") & filters.group)
+async def pause(_, message):
+    await message.delete()
+    user_id = message.from_user.id
+    if user_id != OWNER_ID:
+        return
+    chat_id = message.chat.id
+    if str(chat_id) in CHATS:
+        try:
+            await app.pause_stream(chat_id)
+            await message.reply_text("⏸ Paused streaming.")
+        except:
+            await message.reply_text("❗Nothing is playing.")
+    else:
+        await message.reply_text("❗Nothing is playing.")
+        
+        
+@bot.on_message(filters.command("resume") & filters.group)
+async def resume(_, message):
+    await message.delete()
+    user_id = message.from_user.id
+    if user_id != OWNER_ID:
+        return
+    chat_id = message.chat.id
+    if str(chat_id) in CHATS:
+        try:
+            await app.resume_stream(chat_id)
+            await message.reply_text("⏸ Resumed streaming.")
+        except:
+            await message.reply_text("❗Nothing is playing.")
+    else:
+        await message.reply_text("❗Nothing is playing.")
+        
+        
+@bot.on_message(filters.command("mute") & filters.group)
+async def mute(_, message):
+    await message.delete()
+    user_id = message.from_user.id
+    if user_id != OWNER_ID:
+        return
+    chat_id = message.chat.id
+    if str(chat_id) in CHATS:
+        try:
+            await app.mute_stream(chat_id)
+            await message.reply_text("🔇 Muted streaming.")
+        except:
+            await message.reply_text("❗Nothing is playing.")
+    else:
+        await message.reply_text("❗Nothing is playing.")
+        
+        
+@bot.on_message(filters.command("unmute") & filters.group)
+async def unmute(_, message):
+    await message.delete()
+    user_id = message.from_user.id
+    if user_id != OWNER_ID:
+        return
+    chat_id = message.chat.id
+    if str(chat_id) in CHATS:
+        try:
+            await app.unmute_stream(chat_id)
+            await message.reply_text("🔊 Unmuted streaming.")
+        except:
+            await message.reply_text("❗Nothing is playing.")
+    else:
+        await message.reply_text("❗Nothing is playing.")
+        
+        
+@bot.on_message(filters.command("restart"))
+async def restart(_, message):
+    user_id = message.from_user.id
+    if user_id != OWNER_ID:
+        return
+    await message.reply_text("🛠 <i>Restarting Music Player...</i>")
+    os.system(f"kill -9 {os.getpid()} && python3 app.py")
+            
+
+app.start()
+bot.run()
+idle()
